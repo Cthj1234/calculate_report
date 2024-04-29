22478a4a6c9ced04587ae71303d23760f18f13e5
hanhojin <ghwls8172@naver.com>
2024-04-29 21:02:59 +0900

    Level1 requirement 6 commit

--- src/Calculator/App.java
+++ src/Calculator/App.java
@@ -46,15 +46,22 @@ public class App {
             }
             System.out.println("결과: " + result);
 
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            /* exit을 입력 받으면 반복 종료 */
-            if(Objects.equals(sc.next(), "exit")) break;
 
             /* 연산의 결과를 배열에 저장합니다. */
             int[] answer = new int[10];
             /* index를 증가 시킵니다. */
             if(index < 10) answer[index++] = result;
+            else{
+                for (int i = 0; i < 9; i++) {
+                    int tmp = answer[i + 1];
+                    answer[i] = tmp;
+                }
+                answer[9] = result;
+            }
 
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            /* exit을 입력 받으면 반복 종료 */
+            if(Objects.equals(sc.next(), "exit")) break;
         }
 
     }
